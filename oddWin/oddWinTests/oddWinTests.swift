1e150780f5d211b6c8e848e94096514d7133af03
Keerthika Chokkalingam <142724355+KeerthikaChokkalingam@users.noreply.github.com>
2025-07-16 12:44:29 +0530

    commit with project creation

---
+++ oddWin/oddWinTests/oddWinTests.swift
@@ -0,0 +1,17 @@
+//
+//  oddWinTests.swift
+//  oddWinTests
+//
+//  Created by Keerthika on 16/07/25.
+//
+
+import Testing
+@testable import oddWin
+
+struct oddWinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
